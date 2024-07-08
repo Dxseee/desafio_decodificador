7bb6709e795838178a5ef55ec5696e6963996f5a
Diogo da Anunciação dos Santos <diogodaasantos@gmail.com>
2024-07-07 22:12:57 -0300

    80% terminado

--- script.js
+++ script.js
@@ -4,39 +4,56 @@ A letra "a" é convertida para "ai"
 A letra "o" é convertida para "ober"
 A letra "u" é convertida para "ufat"
 */
+let textCriptografado;
+let textDescriptografado;
 
-function criptografar(){
+const  criptografar = () => {
     let texto = document.getElementById("campoTexto").value;
     if (texto != ""){
         let cripE = texto.replaceAll("e","enter");
         let cripI = cripE.replaceAll("i","imes");
         let cripA = cripI.replaceAll("a","ai");
         let cripO = cripA.replaceAll("o","ober");
-        let textCriptografado = cripO.replaceAll("u","ufat");
-     
-    
-        let element = document.getElementById("imagem").remove();
-    
+        textCriptografado = cripO.replaceAll("u","ufat");
+        
+        removerElements(textCriptografado);
+
         console.log(textCriptografado);
-        return textCriptografado ;
+        return textCriptografado;
+
 
     }
 
     
 }
-function descripitografar(){
+
+function descriptografar(){
+    let descripE = textCriptografado.replaceAll("enter","e");
+    let descripI = descripE.replaceAll("imes","i");
+    let descripA = descripI.replaceAll("ai","a");
+    let descripO = descripA.replaceAll("ober","o");
+    textDescriptografado = descripO.replaceAll("ufat","u");
+
+    console.log(textDescriptografado);
+    return textDescriptografado;
 
 }
 
-function removerImagem(){
+function removerElements(parametro){
+    let elementImagem = document.getElementById("imagem");
 
-}
+    let criarElementTexto = document.createElement("h1");
+    criarElementTexto.classList.add('h1TextoCriptografado');
+    criarElementTexto.innerHTML = parametro;
+    elementImagem.replaceWith(criarElementTexto);
 
-function adicionarBotaoCopiar(){
+    let criarElementButton =  document.createElement("button");
+    criarElementButton.classList.add("buttonCriado");
+    criarElementButton.innerHTML = "Copiar";
 
-}
+    let elementTexto1= document.getElementById("texto1");
+    elementTexto1.replaceWith(criarElementButton);
 
-
-function copiarTexto(){
-
+    let elementTexto2= document.getElementById("texto2").remove();
+    
 }
